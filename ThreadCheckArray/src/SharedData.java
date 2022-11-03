aee3ff206529e7202def2fb68de31782c36e4bd5
ToiznerD <toiznerd@gmail.com>
2022-11-03 11:58:47 +0200

    sdffssaf

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -3,7 +3,7 @@ public class SharedData
 	private int [] array;
 	private boolean [] winArray;
 	private boolean flag;
-	private final int b;//fgfgfdsfsadfdfgdf
+	private final int b;//fgfgfdsfsadfdfgdffsdfsd
 	
 	public SharedData(int[] array, int b) {
 		
